a442f3c735cfe099efd3bbe9b24b46017af44329
Luc <luc.a.h.desautels@gmail.com>
2024-04-15 13:26:21 -0400

    Added more stuff

---
+++ src/Eleve.java
@@ -0,0 +1,25 @@
+/**
+ * In: techclass2023
+ *
+ * @author : lucah
+ * @created : 1:22 p.m. on Monday, April 15, 2024.
+ * @Description :
+ **/
+public class Eleve {
+int nombreEleve;
+
+String nom;
+String prenom;
+String age;
+
+
+public Eleve(int nombreEleve, String nom, String prenom, String age) {
+    this.nombreEleve = nombreEleve;
+    this.nom = nom;
+    this.prenom = prenom;
+    this.age = age;
+}
+public Eleve(int nombreEleve){
+
+}
+}
---
+++ src/HeadingTest.java
@@ -0,0 +1,9 @@
+/**
+ * In: techclass2023
+ *
+ * @author : lucah
+ * @created : 12:42 p.m. on Monday, April 15, 2024.
+ * @Description :
+ **/
+public class HeadingTest {
+}
